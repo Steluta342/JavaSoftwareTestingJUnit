e0a31684a7a1c1c0563e3b5b64b0252b8fc127a0
hp <dragusteluta30@gmail.com>
2025-04-07 21:17:42 +0300

    Proiect JavaTestingJUnit

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import org.example.calculator.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    //Adunarea ar trebui sa fie corecta pt nr intregi
+    @Test
+    public void testAdunare(){
+        System.out.println("S-a intrat in testul pt verificarea adunarii!");
+        double rezultatActual = calculator.adunare(4,5);
+        //Definiti rezultatul actual asteptat -> Double rezultatAsteptat = 9;
+        assertEquals(9, rezultatActual);//assert(expected, actual);
+
+    }
+
+    @Test
+    public void testScadere(){
+        System.out.println("S-a intrat in testul pt verificarea scaderii!");
+        double rezultatActual = calculator.scadere(4,5);
+        //Definiti rezultatul actual asteptat -> Double rezultatAsteptat = 9;
+        assertEquals(-1, rezultatActual);//assert(expected, actual);
+
+    }
+
+}
