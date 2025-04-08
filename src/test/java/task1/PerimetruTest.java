a20ce7fc52ed3702765a9b5f3aca52cd6c49d797
hp <dragusteluta30@gmail.com>
2025-04-08 19:53:41 +0300

    Am adaugat test la clasa perimetru

---
+++ src/test/java/task1/PerimetruTest.java
@@ -0,0 +1,69 @@
+package task1;
+
+import org.example.task1.Perimetru;
+import org.junit.jupiter.api.Test;
+
+import java.time.Period;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PerimetruTest {
+
+    /* Testul functional - de regulase utilizeaza date de test pentru un happy flow comun */
+    @Test
+    public void testPerimetruHappyFlow() {
+        Perimetru perimetru = new Perimetru();
+
+        //se poate defini parametru ca variabila a metodei de test
+        //astfel incat sa fie pasat la definirea celor doua variabile de rezultat
+
+        double rezultatActual = perimetru.calculeazaPerimetru(2.0);
+        double rezultatAsteptat = 2.0 * Math.PI;
+
+        assertEquals(rezultatAsteptat, rezultatActual, 0.0001);
+
+    }
+
+    @Test
+    /* Boundary test -> verifica valori in zona lui 0, empty sau null */
+    public void testPerimetruZero() {
+        Perimetru perimetru = new Perimetru();
+
+        double rezultatActual = perimetru.calculeazaPerimetru(0.0);
+
+        assertEquals(0.0, rezultatActual, 0.0001);
+    }
+
+    /* Negative test / Test de negativ -> verifica volori in zona numerelor negative */
+
+    @Test
+    public void testPerimetruNumareNegative() {
+        Perimetru perimetru = new Perimetru();
+        double rezultatActual = perimetru.calculeazaPerimetru(-2.0);
+        assertEquals(-2.0 * Math.PI, rezultatActual, 0.0001);
+    }
+
+    /*  si test de precizie -> verifica valori fractionare  */
+    @Test
+    public void testPerimetruPrecizie() {
+        Perimetru perimetru = new Perimetru();
+        double rezultatActual = perimetru.calculeazaPerimetru(1.5);
+        assertEquals(4.71239, rezultatActual, 0.0001);
+    }
+
+    /*Scaling Test -> verifica valori intr-o plaja de multiplicari */
+    @Test
+    public void testPerimetruScalat() {
+        Perimetru perimetru = new Perimetru();
+
+        double diametru1 = 5.0;
+        double diametru2 = 2 * diametru1;
+
+        double rezultatAsteptatP1 = perimetru.calculeazaPerimetru(diametru1);
+        double rezultatAsteptatP2 = perimetru.calculeazaPerimetru(diametru2);
+        assertEquals(rezultatAsteptatP1 * 2, rezultatAsteptatP2, 0.0001 );
+    }
+
+
+}
+
